73988b824abca60e87f2cb33c174d2d4c19c7bf0
RoyOkite <royokite@gmail.com>
2022-09-21 16:56:17 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -13,11 +13,7 @@ function ColorList() {
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colors.map(color => <li style={{color: color}}>{color}</li>)}
       </ol>
     </div>
   );
